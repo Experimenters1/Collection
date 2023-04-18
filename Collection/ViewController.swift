18539ee18a35934c31cd553858a5e046cf7714e0
Huy Vu <64000769+Experimenters1@users.noreply.github.com>
2023-04-18 14:18:54 +0700

    First commit

---
+++ Collection/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Collection
+//
+//  Created by huy on 18/04/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        200
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "sdfsdf", for: indexPath) as! CollectionViewCell
+        return cell
+    }
+    
+
+    @IBOutlet weak var col: UICollectionView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        col.register(UINib(nibName: "CollectionViewCell", bundle: .main), forCellWithReuseIdentifier: "sdfsdf")
+        
+        
+        let margin: CGFloat = 12
+                    let layout = UICollectionViewFlowLayout()
+                    layout.scrollDirection = .vertical
+                    layout.minimumLineSpacing = margin
+                    layout.minimumInteritemSpacing = margin
+                    var sizeCell = (view.frame.size.width - 4 * margin) / 3 - 2
+                    if UIDevice.current.userInterfaceIdiom == .pad {
+                        sizeCell = (view.frame.size.width - 5 * margin) / 4
+                    }
+                    
+                    layout.itemSize = CGSize(width: sizeCell, height: sizeCell)
+                    layout.sectionInset = UIEdgeInsets.init(top: margin, left: margin, bottom: margin, right: margin)
+        col.collectionViewLayout = layout
+    }
+
+
+}
+
